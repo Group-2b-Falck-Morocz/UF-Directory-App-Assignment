78a7511b6a49638fe5bf2736873316f31117ba51
Samantha Morocz <samanthamorocz@gmail.com>
2016-01-13 16:42:51 -0500

    Completed JavaScript of Assignment 2

--- listingController.js
+++ listingController.js
@@ -1,14 +1,40 @@
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
     $scope.listings = Listings;
+    $scope.searchText = '';
     $scope.detailedInfo = undefined;
-
+    $scope.showNewListingForm = false;
+    $scope.newListing = {
+          'code': '', 
+            'name': '', 
+            'coordinates': {
+                'latitude': 0, 
+                'longitude': 0
+            }, 
+            'address': ''
+    };
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      $scope.listings.unshift($scope.newListing);
+      $scope.newListing = {
+        'code': '', 
+            'name': '', 
+            'coordinates': {
+                'latitude': 0, 
+                'longitude': 0
+            }, 
+            'address': ''
+      }
+      $scope.showNewListingForm=false;
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
