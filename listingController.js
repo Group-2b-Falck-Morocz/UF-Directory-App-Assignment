9e53d35e98d1ed41d790d6249c4c40aa902f44c9
Christopher Falck <christopher.falck@gmail.com>
2016-01-14 21:56:37 -0500

    Revert "Complete JavaScript Assignment2 Merge" Merged did not complete succesfully, reverting. This reverts commit 0fe7d37dcd80365ca84e9cee2e64ab3d9d111e44, reversing changes made to 78a7511b6a49638fe5bf2736873316f31117ba51.

--- listingController.js
+++ listingController.js
@@ -5,7 +5,6 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.detailedInfo = undefined;
     $scope.showNewListingForm = false;
     $scope.newListing = {
-<<<<<<< HEAD
           'code': '', 
             'name': '', 
             'coordinates': {
@@ -13,22 +12,12 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
                 'longitude': 0
             }, 
             'address': ''
-=======
-        'code': '', 
-        'name': '', 
-        'coordinates': {
-            'latitude': 0, 
-            'longitude': 0
-        }, 
-        'address': ''
->>>>>>> f5c4ec36970a6a4f6297fed033a3737f89cf11e9
     };
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
     $scope.addListing = function() {
-<<<<<<< HEAD
       $scope.listings.unshift($scope.newListing);
       $scope.newListing = {
         'code': '', 
@@ -46,25 +35,6 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     };
     $scope.showDetails = function(index) {
       $scope.detailedInfo = $scope.listings[index];
-=======
-    	$scope.listings.unshift($scope.newListing);
-    	$scope.newListing = {
-	        'code': '', 
-	        'name': '', 
-	        'coordinates': {
-	            'latitude': 0, 
-	            'longitude': 0
-	        }, 
-	        'address': ''
-	    };
-	    $scope.showNewListingForm = false;
-    };
-    $scope.deleteListing = function(index) {
-    	$scope.listings.splice(index, 1);
-    };
-    $scope.showDetails = function(index) {
-    	$scope.detailedInfo = $scope.listings[index];
->>>>>>> f5c4ec36970a6a4f6297fed033a3737f89cf11e9
     };
   }
 ]);
